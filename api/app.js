d7bf3946bb25b405278e2f652e27e3aba90baac2
WebMats <lorlando127@gmail.com>
2019-03-08 13:10:34 -0500

    added test to delete game

--- api/app.js
+++ api/app.js
@@ -1,19 +1,22 @@
 const express = require('express');
-const app = express();
 const gamesDB = require('../games/gamesModel');
 
-app.get('/', (req, res, next) => {
+const app = express();
+app.use(express.json());
+
+app.get('', (req, res, next) => {
     gamesDB.getAll().then(games => {
         res.status(200).json(games)
     })
 })
-app.post('/', (req, res, next) => {
+
+app.post('', (req, res, next) => {
     const {title, genre} = req.body;
     if (!title || !genre) {
         return res.status(422).json({errorMessage: "Please provide both a title and a genre."})
     }
     try {
-        
+        res.sendStatus(200)
     } catch (err) {
         console.log(err)
         res.status(500).json({errorMessage: "Could not add game to database."})
--- api/app.test.js
+++ api/app.test.js
@@ -9,26 +9,31 @@ test('should return a list and a status of 200', async () => {
     expect(Array.isArray(games)).not.toBeFalsy();
 })
 
-test('should return game added', () => {
-    const newGame = {title: 'Super Smash Bros. Ultimate', genre: 'Fighting', releaseYear: 2018}
-    const result = await request(app).post('/').set('Content-Type', 'application/json').send(JSON.stringify(newGame))
-    const {status, body} = result;
-    id = body.id;
-    const receivedObject = {title: body.title, genre: body.genre, releaseYear: body.releaseYear};
-    expect(status).toEqual(201);
-    expect(receivedObject).toMatchObject(newGame);
+// test('should return game added', async () => {
+//     const newGame = {title: 'Super Smash Bros. Ultimate', genre: 'Fighting', releaseYear: 2018}
+//     const result = await request(app).post('/').set('Content-Type', 'application/json').send(JSON.stringify(newGame))
+//     const {status, body} = result;
+//     id = body.id;
+//     const receivedObject = {title: body.title, genre: body.genre, releaseYear: body.releaseYear};
+//     expect(status).toEqual(201);
+//     expect(receivedObject).toMatchObject(newGame);
+// })
+test('should delete the game with right id', () => {
+    const result = await request(app).delete(`/${id}`);
+    expect(result.status).toEqual(201);
+    expect(result.body.message).toMatch(/deleted/);
 })
 
 test('should return 422 status due to missing title', async () => {
     const newGame = {genre: 'Fighting', releaseYear: 2018}
-    const result = await request(app).post('/').set('Content-Type', 'application/json').send(JSON.stringify(newGame))
+    let result = await request(app).post('/').set('Content-Type', 'application/json').send(JSON.stringify(newGame));
     const {status, body} = result;
     expect(status).toEqual(422);
     expect(body.errorMessage).toMatch(/Please provide both a title and a genre./);
 })
 test('should return 422 status due to missing genre', async () => {
     const newGame = {title: 'Super Smash Bros. Ultimate', releaseYear: 2018}
-    const result = await request(app).post('/').set('Content-Type', 'application/json').send(JSON.stringify(newGame))
+    const result = await request(app).post('/').set('Content-Type', 'application/json').send(JSON.stringify(newGame));
     const {status, body} = result;
     expect(status).toEqual(422);
     expect(body.errorMessage).toMatch(/Please provide both a title and a genre./);
